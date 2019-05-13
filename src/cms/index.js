c33db748672eabc411cf8760178bdcbbea3b06b7
laihongchang <laihongchang@daixiaomi.com>
2019-05-14 02:46:12 +0800

    test netlify

--- src/cms/index.js
+++ src/cms/index.js
@@ -3,5 +3,5 @@ import CMS from 'netlify-cms';
 import PagePreview from './preview-templates/page-preview';
 import PostPreview from './preview-templates/post-preview';
 
-CMS.registerPreviewTemplate('pages', PagePreview);
-CMS.registerPreviewTemplate('posts', PostPreview);
+// CMS.registerPreviewTemplate('pages', PagePreview);
+// CMS.registerPreviewTemplate('posts', PostPreview);
